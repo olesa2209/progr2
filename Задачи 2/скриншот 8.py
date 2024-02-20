e9a0c1c603273e17b0f9b4282b44bee7716d8455
olesa2209 <147470872+olesa2209@users.noreply.github.com>
2024-02-20 10:26:40 +0500

    Add files via upload

---
+++ Задачи 2/скриншот 8.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 17 11:23:31 2024
+
+@author: Pupil
+"""
+import math
+class Point:
+    def __init__(self, x1,y1,x2,y2):
+        self.x2 = x2
+        self.y2 = y2
+        self.x1 = x1
+        self.y1 = y1
+        
+    def rasstoinie(self):
+        a=math.sqrt((self.x2-self.x1)**2+(self.y2-self.y1)**2)
+        print("расстояние между точками -",a)
+        
+    def os(self):
+        if self.x1==0:
+            print('точка 1 лежит на оси OY')
+        if self.x2==0:
+            print('точка 2 лежит на оси OY')
+        if self.y1==0:
+            print('точка 1 лежит на оси OX')
+        if self.y2==0:
+            print('точка 1 лежит на оси OX')
+     
+    def peremeshenie(self):
+        b=math.sqrt((self.x2-self.x1)**2+(self.y2-self.y1)**2)
+        print("модуль перемещения -",b)
+          
+point1 = Point(1,2,5,7)
+point2 = Point(6,0,0,7)
+
+point2.os()

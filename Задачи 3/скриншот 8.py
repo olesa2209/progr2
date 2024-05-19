665ea9cff137702e937058f831d31db8ba9bc229
olesa2209 <147470872+olesa2209@users.noreply.github.com>
2024-05-19 20:50:36 +0500

    Add files via upload

---
+++ Задачи 3/скриншот 8.py
@@ -0,0 +1,8 @@
+nums = [int(x) for x in input().split()]
+n = 1
+while n < len(nums):
+   for i in range(len(nums) - n):
+       if nums[i] > nums[i + 1]:
+           nums[i], nums[i + 1] = nums[i + 1], nums[i]
+   n += 1
+print(nums)
